c314af1af78663fad416058cb738f4a855a7f70c
cheershema@gmail.com <cheershema@gmail.com>
2019-03-17 21:29:49 -0400

    documents added

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -11,8 +11,8 @@ export class VideoComponent implements OnInit {
   youtubeUrl = 'https://www.youtube.com/watch?v=iHhcHTlGtRs';
   dailymotionUrl =
     'https://www.dailymotion.com/video/x20qnej_red-bull-presents-wild-ride-bmx-mtb-dirt_sport';
- 
-  vimeoId = '197933516';
+
+  vimeoId = '197933511';
   youtubeId = 'iHhcHTlGtRs';
   dailymotionId = 'x20qnej';
 
@@ -23,7 +23,7 @@ export class VideoComponent implements OnInit {
     console.log(this.embedService.embed(this.vimeoUrl));
     console.log(this.embedService.embed(this.youtubeUrl));
     console.log(this.embedService.embed(this.dailymotionUrl));
- 
+
     console.log(this.embedService.embed_vimeo(this.vimeoId));
     console.log(this.embedService.embed_youtube(this.youtubeId));
     console.log(this.embedService.embed_dailymotion(this.dailymotionId));
